3786aecec0e41c8d2cb3c8b63d882410f92f8f5d
wigsters52 <ryanwiggins52@gmail.com>
2021-01-30 15:53:42 -0500

    card added

--- Github Profiles/script.js
+++ Github Profiles/script.js
@@ -1,15 +1,42 @@
 const APIURL = 'https://api.github.com/users/'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+
 
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username)
-        console.log(data)
+        createUserCard(data)
     } catch (err) {
-        console.log(err)
+        if (err.response.status = 404) {
+            createErrorCard('No profile with this user name')
+        }
+
     }
+
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHTML
 
 }
 
@@ -23,3 +50,13 @@ form.addEventListener('submit', (e) => {
         search.value = ''
     }
 })
+
+function createErrorCard(msg) {
+    const cardHTML = `
+    <div class="card">
+        <h1>${msg}</h1>
+    </div>
+    `
+
+    main.innerHTML = cardHTML
+}
